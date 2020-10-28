ccfe07353cba1b781f448acc6fd43ab0ce2c5b95
elder2701 <elder111@yandex.ru>
2020-10-28 17:27:49 +0300

    оживил модальные окна

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+/*Карта*/ 
+const map = document.querySelector('.info-contact__map');
+const mapModal = document.querySelector('.modal__map');
+const mapCloseBtn = document.querySelector('.modal-map__cross');
+
+map.addEventListener('click', () => {
+  mapModal.classList.add("modal__map--show");
+});
+
+mapCloseBtn.addEventListener('click', () => {
+  mapModal.classList.remove("modal__map--show");
+});
+
+/*Форма обратной связи*/
+
+const writeUsBtn = document.querySelector('.info-contact__letter');
+const formModal = document.querySelector('.modal__contact');
+const formCloseBtn = document.querySelector('.modal-contact__cross');
+
+writeUsBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  formModal.classList.add("modal__contact--show");
+});
+
+formCloseBtn.addEventListener('click', () => {
+  formModal.classList.remove("modal__contact--show");
+});
---
+++ js/slider.js
@@ -0,0 +1,6 @@
+/*Главный слайдер*/
+
+const mainSliderBtns = document.getElementsByClassName('slider-nav__item');
+console.log(mainSliderBtns)
+
+/*Слайдер с услугами*/
